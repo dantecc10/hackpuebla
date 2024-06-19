9734da27b07b27624a04ab568c5d8756d4243130
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-06-19 05:22:53 -0600

    api done

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-echo (isset($_POST['name']) && isset($_POST['last_names'])) ? ("Hola " . $_POST['name'] . " " . $_POST['last_names']) : 'No proporcionaste suficientes datos.';
+//echo (isset($_POST['name']) && isset($_POST['last_names'])) ? ("Hola " . $_POST['name'] . " " . $_POST['last_names']) : 'No proporcionaste suficientes datos.';
+
+if (
+    isset($_POST['auth']) &&
+    isset($_POST['id']) &&
+    isset($_POST['header']) &&
+    isset($_POST['state'])
+) {
+    include('connection.php');
+    $auth = $_POST['auth'];
+    $id = $_POST['id'];
+    $state = $_POST['state'];
+    $header = $_POST['header'];
+    if ($auth == '54dsnv8s7hghdf4i7bdgsuASDLHs') {
+        $sql = "INSERT INTO `news` VALUES('', $id, '$header', '$state');";
+        if ($conn->query($sql) === TRUE) {
+            echo ("Success");
+            //echo "Noticia creada correctamente";
+        } else {
+            echo ("Error");
+            //echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else {
+        echo ("Fatal");
+    }
+} else {
+    echo ("Fatal");
+}
--- requester.py
+++ requester.py
@@ -1,15 +1,18 @@
 import requests
 
-url = 'https://compupandilla.castelancarpinteyro.com/web-repo/'#?name=Dante&last_names=Castelan%20Carpinteyro'
 
-# Hacer una solicitud POST
-response = requests.post(url, data={'name': 'Dante', 'last_names': 'Castelan Carpinteyro'})
-#response = requests.get(url)
+def upload_data(id, header, state):
+    auth = '54dsnv8s7hghdf4i7bdgsuASDLHs'
+    url = 'https://compupandilla.castelancarpinteyro.com/web-repo/'#?name=Dante&last_names=Castelan%20Carpinteyro'
+
+    # Hacer una solicitud POST
+    response = requests.post(url, data={'auth': auth,'id': id, 'header': header, 'state': state})
+    #response = requests.get(url)
 
 
-# Verificar si la solicitud fue exitosa
-if response.status_code == 200:
-    print('Solicitud exitosa')
-    print('Contenido:', response.text)
-else:
-    print('Error en la solicitud:', response.status_code)
+    # Verificar si la solicitud fue exitosa
+    if response.status_code == 200:
+        print('Solicitud exitosa')
+        print('Contenido:', response.text)
+    else:
+        print('Error en la solicitud:', response.status_code)
